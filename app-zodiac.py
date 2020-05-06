b41d4c0a72a1367b8d5e4f1a3bb6cb607fbc7aea
Kattie Way <kattievoronina@gmail.com>
2020-05-06 17:18:16 +0200

    frutty tutty

---
+++ app-zodiac.py
@@ -0,0 +1,30 @@
+fruits = ['banana', 'strawberry', 'kiwi']
+
+if 'banana' in fruits:
+    print('you really like banana')
+
+if 'strawberry' in fruits:
+    print('you really like strawberry')
+
+if 'kiwi' in fruits:
+    print('you really like kiwi')
+
+else:
+    print('go and fuck youself, you fruit is not in the list')
+
+requesred_toppings = ['muchrooms', 'tomato', 'onion']
+for requesred_topping in requesred_toppings:
+    if requesred_topping == 'tomato':
+        print(f"{requesred_topping} is in my ass")
+    else:
+        print(f"{requesred_topping} is added")
+
+
+toppings = []
+if toppings:
+    for topping in toppings:
+        print(f"adding{topping}")
+else:
+    print('are you sure you are not an idiot?')
+
+
